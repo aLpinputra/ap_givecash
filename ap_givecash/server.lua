5e7ed9e308ecf947aeda8b1fe1bb54d08dfba987
aLpin putra <102750104+aLpinputra@users.noreply.github.com>
2024-03-20 05:09:43 +0700

    Update server.lua
    remove alert

--- ap_givecash/server.lua
+++ ap_givecash/server.lua
@@ -12,6 +12,12 @@ GetSource = function(Player)
     return Config.Core == 'esx' and Player.source or Config.Core == 'qb' and Player.PlayerData.source
 end
 
+GetCoords = function(target)
+    local Playerpos = GetEntityCoords(GetPlayerPed(source))
+    local Targetpos = GetEntityCoords(GetPlayerPed(target))
+    return #(Playerpos - Targetpos) < 3.0
+end
+
 MoneyAction = function(Player, amount, type)
     if type == 'get' then
         return Config.Core == 'esx' and Player.getAccount('money').money or Config.Core == 'qb' and Player.Functions.GetMoney('cash')
@@ -26,33 +32,18 @@ lib.callback.register('ap_givecash:getPlayerMoney', function(source)
     return MoneyAction(GetPlayerId(source), nil, 'get')
 end)
 
-RegisterServerEvent('ap_givecash:checkPlayer')
-AddEventHandler('ap_givecash:checkPlayer', function(data)
+RegisterServerEvent('ap_givecash:sendMoney')
+AddEventHandler('ap_givecash:sendMoney', function(data)
 	local Player = GetPlayerId(source)
 	local Target = GetPlayerId(data.target)
     if Player == nil or Target == nil then
         TriggerClientEvent('ap_givecash:sendNotify', GetSource(Player), 'ID '..GetSource(Target)..' Offline!', 'error')
         return
     end
-    data.senderid = GetSource(Player)
-    TriggerClientEvent('ap_givecash:sendAlert', GetSource(Target), data)
-end)
-
-RegisterServerEvent('ap_givecash:sendMoney')
-AddEventHandler('ap_givecash:sendMoney', function(data, confirm)
-	local Player = GetPlayerId(source)
-	local Target = GetPlayerId(data.senderid)
-    if Player == nil or Target == nil then
-        TriggerClientEvent('ap_givecash:sendNotify', GetSource(Player), 'ID '..GetSource(Target)..' Offline!', 'error')
-        return
-    end
-    if confirm == 'confirm' then
-        MoneyAction(Target, data.amount, 'remove')
-        MoneyAction(Player, data.amount, 'add')
-        TriggerClientEvent('ap_givecash:sendNotify', GetSource(Target), 'ID '..GetSource(Player)..' Accept!', 'success')
-    elseif confirm == 'cancel' then
-        TriggerClientEvent('ap_givecash:sendNotify', GetSource(Target), 'ID '..GetSource(Player)..' Reject!', 'error')
-    else
-        TriggerClientEvent('ap_givecash:sendNotify', GetSource(Target), 'ID '..GetSource(Player)..' Is busy!!', 'inform')
-    end
+    if Player == Target then return end
+    if not GetCoords(data.target) then return end
+    MoneyAction(Player, data.amount, 'remove')
+    MoneyAction(Target, data.amount, 'add')
+    TriggerClientEvent('ap_givecash:sendNotify', GetSource(Player), 'You given $' .. lib.math.groupdigits(data.amount) .. (data.note ~= '' and '  \nNotes : ' .. data.note or ''), 'success')
+    TriggerClientEvent('ap_givecash:sendNotify', GetSource(Target), 'You receive money amounting to $' .. lib.math.groupdigits(data.amount) .. (data.note ~= '' and '  \nNotes : ' .. data.note or ''), 'success')
 end)
